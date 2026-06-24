c9f576546c90d41fd6e4373f72d0c6cdc76d7d00
kimsseTheWolf <ilovemcmrcreeper@gmail.com>
2026-06-24 22:29:27 +0800

    Remove test components

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import { createBiliAudioCore } from "./scripts/biliAudioCore"
-
-const audioFetchCore = createBiliAudioCore({
-  includeCredentials: false,
-  requestTimeoutMs: 12000,
-  apiOrigin: "/api/bili"
-})
-
-async function simpleFetchAudioExample() {
-  const source = audioFetchCore.parseSourceOrThrow('BV1Sr4y1d7EF');
-  const songs = await audioFetchCore.getSongsFromSource(source);
-  const play = await audioFetchCore.resolveSongAudioUrl(songs[0]);
-
-  console.log(play.url)
-}
 
 createRoot(document.getElementById('root')!).render(
-  <button onClick={simpleFetchAudioExample}>Click here to test</button>
+  <>
+    
+  </>
 )
